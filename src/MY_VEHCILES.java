d4e0a73074193d197ab1df1a267b268c96ca56dc
Sakib <sakib.safwan@gmail.com>
2023-11-17 16:03:09 +0600

    adil er sathe ........ er biyete kacchi chai

--- src/MY_VEHCILES.java
+++ src/MY_VEHCILES.java
@@ -4,6 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import java.sql.*;
+import javax.swing.table.DefaultTableModel;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -26,6 +27,7 @@ public class MY_VEHCILES extends javax.swing.JFrame {
     public MY_VEHCILES(int xxx){
         initComponents();
         UID = xxx;
+        DefaultTableModel model = (DefaultTableModel) NWT;
     }
     
     Connection con;
@@ -41,7 +43,7 @@ public class MY_VEHCILES extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        NWT = new javax.swing.JTable();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -55,7 +57,7 @@ public class MY_VEHCILES extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        NWT.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -63,10 +65,10 @@ public class MY_VEHCILES extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Type", "VehicleID", "Brand", "Days Remaining"
+                "Type", "VehicleID", "Brand", "Availability"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(NWT);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -277,6 +279,7 @@ public class MY_VEHCILES extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable NWT;
     private javax.swing.JButton Save_Vehicle;
     private javax.swing.JRadioButton Set_Vehicle;
     private javax.swing.JTextField Vehicle_ID;
@@ -288,6 +291,5 @@ public class MY_VEHCILES extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
